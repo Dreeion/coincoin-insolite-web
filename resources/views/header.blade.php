6f5af5ff25f0cd1573b07a88f310a6a5cf8527ec
Axelle <guinaudeau.ion1516@gmail.com>
2019-12-18 09:44:54 +0100

    start !

---
+++ resources/views/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <p class="title">A new world</p>
+    <p class="download">
+        <span>Download APK</span>
+        <span><img src="{{ asset('img/icon/download.png') }}" alt="download"></span>
+    </p>
+</header>
