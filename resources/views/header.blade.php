4e655df20cdeea64257438bacaa357675d51d457
maeldebon <mael.dbn@gmail.com>
2019-12-18 14:17:56 +0100

    Download button, texts and typo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,21 @@
 <header>
-    <p class="title">A new world</p>
-    <p class="download">
-        <span>Download APK</span>
-        <span><img src="{{ asset('img/icon/download.png') }}" alt="download"></span>
-    </p>
+    <p class="title">Find unusual landscapes around you!</p>
+    <div class="dl_div">
+        <div class="dl_link">
+            <a href="{{ asset('file.csv') }}">
+                <p class="download">
+                    <span>Download for Android</span>
+                    <span><img src="{{ asset('img/icon/download.png') }}" alt="download" class="download_button"></span>
+                </p>
+            </a>
+        </div>
+        <div class="dl_link2">
+            <a href="{{ asset('file.csv') }}">
+                <p class="download">
+                    <span>Download for iOS</span>
+                    <span><img src="{{ asset('img/icon/download.png') }}" alt="download" class="download_button"></span>
+                </p>
+            </a>
+        </div>
+    </div>
 </header>
