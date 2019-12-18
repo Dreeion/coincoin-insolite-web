92236c775e11d55ba6f318e41046e74007a39978
Axelle <guinaudeau.ion1516@gmail.com>
2019-12-18 14:19:06 +0100

    add asset

--- resources/views/content_comment.blade.php
+++ resources/views/content_comment.blade.php
@@ -1,5 +1,29 @@
 <section class="content_comment"><br><br>
-
-    bla<br><br><br><br><br><br><br><br><br><br><br><br><br>
-
+    <section class="white_square_1"><br><br>
+        <img class ="img_1" src="{{ asset('img/photo_profil_1.png') }}" alt="photo_profil" /><br>
+        <img src="{{ asset('img/icon/star.svg') }}" alt="first_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="second_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="third_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="fourth_start" />
+        <img src="{{ asset('img/icon/star_half.svg') }}" alt="fifth_start" /><br><br>
+        <p>Très utile, cela m’a permis de découvrir de nouveaux endroits magnifique près de chez moi.</p>
+    </section>
+    <section class="white_square_2"><br><br>
+        <img class ="img_1" src="{{ asset('img/photo_profil_2.png') }}" alt="photo_profil" /><br>
+        <img src="{{ asset('img/icon/star.svg') }}" alt="first_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="second_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="third_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="fourth_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="fifth_start" /><br><br>
+        <p>Cette application m’a permis de nouveaux lieux unique que je n’aurais jamais découvert.</p>
+    </section>
+    <section class="white_square_3"><br><br>
+        <img class ="img_1" src="{{ asset('img/photo_profil_3.png') }}" alt="photo_profil" /><br>
+        <img src="{{ asset('img/icon/star.svg') }}" alt="first_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="second_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="third_start" />
+        <img src="{{ asset('img/icon/star.svg') }}" alt="fourth_start" />
+        <img src="{{ asset('img/icon/star_outline.svg') }}" alt="fifth_start" /><br><br>
+        <p>Simple à prendre en main, j’ai pu partager de nombreux endroit que j’apprécie méritant d’être connu.</p>
+    </section><br><br>
 </section>
