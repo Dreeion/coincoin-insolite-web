8bd0cf3946ea1ee940fb4e789a24b27e3d5f547e
maeldebon <mael.dbn@gmail.com>
2019-12-18 16:00:34 +0100

    Responsive header & remove hamburger menu

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,5 @@
 <nav>
     <h2>
-        <img id="menu" src="{{ asset('img/icon/menu.svg') }}" alt="">
         <img class="logo-coincoin" src="{{ asset("img/logo_coincoins.png") }}" alt="logo coincoins">
         <p>CoinCoins Insolites </p>
     </h2>
