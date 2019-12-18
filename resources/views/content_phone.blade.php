cecb96f332fca4c291f17d1d1bcbf8bf7925a5a5
Axelle <guinaudeau.ion1516@gmail.com>
2019-12-18 12:00:34 +0100

    add phone

--- resources/views/content_phone.blade.php
+++ resources/views/content_phone.blade.php
@@ -1,3 +1,10 @@
 <section class="content_phone">
-
+    <div class="text">
+        <h3 class="title">Take part of the adventure</h3>
+        <p class="download">
+            <span>Download APK</span>
+            <span><img src="{{ asset('img/icon/download.png') }}" alt="download"></span>
+        </p>
+    </div>
+    <span class="phone"><img src="{{ asset("/img/phone.png") }}" alt="phone"></span>
 </section>
