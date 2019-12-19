78130778110ed8b0325bf493ceab9a7c0c27db69
Axelle <guinaudeau.ion1516@gmail.com>
2019-12-19 15:59:36 +0100

    add lang

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'download_android' => 'Download for android',
+    'title' => 'A new World'
+];
---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'download_android' => 'Télécharger pour android',
+    'header.title' => 'Un nouveau monde'
+];
