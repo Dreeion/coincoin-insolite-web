849adcb30ad5e6bca06b6fe149e86565685def1a
Axelle <guinaudeau.ion1516@gmail.com>
2019-12-19 16:36:01 +0100

    add files trad fr and en

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -1,6 +1,31 @@
 <?php
 
 return [
+    //Header
+    'slogan' => 'Find unusual landscapes around you!',
     'download_android' => 'Télécharger pour android',
-    'header.title' => 'Un nouveau monde'
+    'download_ios' => 'Télécharger pour ios',
+
+    //Discover
+    'discover' => 'DÉCOUVREZ.',
+    'discover_text' => 'Partez à l’aventure des coins peu connus. Que ce soit en vacances ou près de chez vous, ces lieux ne demandent qu’à être visités !',
+    'enjoy' => 'PROFITEZ.',
+    'enjoy_text' => 'Explorez de nouveaux endroits, profitez du lieu, et découvez de nouveaux points d\'intérêt.',
+    'share' => 'PARTAGEZ.',
+    'share_text' => 'Partagez les lieux à visiter près de chez vous et faites-les découvrir à d\'autres utilisateurs !',
+
+    //comment
+    'user1' => 'Je recommande ! CoinCoins Insolites m\'a permis de découvrir de nouveaux lieux magnifiques près de chez moi.',
+    'user2' => 'J\'ai pu facilement organiser mon voyage au Pérou en recherchant au préalable les lieux à visiter sur place !',
+    'user3' => 'Simple à prendre en main, j’ai pu partager de nombreux points d\'intérêts dans ma ville.',
+
+    //join
+    'join' => 'JOIN THE ADVENTURE NOW!',
+
+    //footer
+    'cgu' => 'Terms & privacy',
+    'contact' => 'Contact us',
+    'we' => 'We',
+    'feedback' => 'feedback' ,
+
 ];
