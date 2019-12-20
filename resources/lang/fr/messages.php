9cb1c24d37f07a36bfb8c958afa3fcba42a67723
maeldebon <mael.dbn@gmail.com>
2019-12-20 11:22:40 +0100

    Fix syntax in french translations

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -2,7 +2,7 @@
 
 return [
     //Header
-    'slogan' => 'Découvrez des paysages insolites autour de vous!',
+    'slogan' => 'Découvrez des paysages insolites autour de vous !',
     'download_android' => 'Télécharger pour Android',
     'download_ios' => 'Télécharger pour iOS',
 
@@ -20,7 +20,7 @@ return [
     'user3' => 'Simple à prendre en main, j’ai pu partager de nombreux points d\'intérêts dans ma ville.',
 
     //join
-    'join' => 'REJOIGNEZ l\'AVENTURE MAINTENANT!',
+    'join' => 'REJOIGNEZ l\'AVENTURE MAINTENANT !',
 
     //footer
     'cgu' => 'Conditions d\'utilisation',
