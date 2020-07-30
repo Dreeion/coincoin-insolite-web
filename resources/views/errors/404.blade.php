f55b73c04c21f6d109574b13d9b1221606623908
axelle <guinaudeau.ion1516@gmail.com>
2020-07-30 15:30:55 +0200

    Ajout de la page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('img/logo_coincoins_tab.png')}}" />
+    <title>CoinCoins</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div class="not-found" style="background-image: url('{{ asset('/img/404/fond404.jpg')}}');">
+    <h1>{{ __('messages.not_found_title') }}</h1>
+    <p>{{ __('messages.not_found_text1') }}</p>
+    <p>{{ __('messages.not_found_text2') }}</p>
+
+    <p>{{ __('messages.not_found_return_home') }}<a href="{{route('home')}}">{{route('home')}}</a></p>
+</div>   
+</body>
+</html>
+
+
+
+
+
